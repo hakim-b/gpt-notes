29da56a3ff8fde82111c003eefbaffe3274d70aa
hakim-b <hakimbouabdellah02@gmail.com>
2024-01-31 15:12:27 -0500

    another type error

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,5 +1,6 @@
 import NoteCard from "@/components/note-card";
 import { authOptions } from "@/lib/auth";
+import { Note } from "@prisma/client";
 import { Metadata } from "next";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
@@ -22,7 +23,7 @@ async function Notes() {
   return (
     <>
       <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3">
-        {allNotes?.map((note) => <NoteCard note={note} key={note.id} />)}
+        {allNotes?.map((note: Note) => <NoteCard note={note} key={note.id} />)}
       </div>
       {allNotes?.length === 0 && (
         <h2 className="col-span-full text-center text-2xl font-bold">
