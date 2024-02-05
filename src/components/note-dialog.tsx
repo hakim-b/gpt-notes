7c9facfada6924dde0fd03a68a2d8b952f3db72c
Hakim Bouabdellah <56936628+hakim-b@users.noreply.github.com>
2024-02-05 10:39:36 -0500

    Update note-dialog.tsx

--- src/components/note-dialog.tsx
+++ src/components/note-dialog.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { CreateNote, createNoteSchema } from "~/types/notes-crud";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
